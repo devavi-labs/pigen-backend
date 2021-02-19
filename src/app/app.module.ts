fd0b8d56d0d66d186069a87ce9f7d40173265b8d
Avinash Sinha <savinash2608@gmail.com>
2021-02-19 23:01:56 +0530

    in-memory schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common"
 import { GraphQLModule } from "@nestjs/graphql"
-import { join } from "path"
 import { AppResolver } from "./app.resolver"
 import { AppService } from "./app.service"
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      autoSchemaFile: true,
     }),
   ],
   providers: [AppResolver, AppService],
