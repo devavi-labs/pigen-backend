4d4db5dade34d05e8adaa9107f4566c0455cb620
Avinash Sinha <savinash2608@gmail.com>
2021-02-20 13:17:50 +0530

    logging added

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,6 +1,6 @@
 import { AppService } from "./app.service"
 import { ConfigService } from "@nestjs/config"
-import { Controller } from "@nestjs/common"
+import { Controller, Logger } from "@nestjs/common"
 import { APP, AppConfig } from "src/config/app.config"
 import { AppDto } from "./dto/app.dto"
 
@@ -13,16 +13,24 @@ export class AppController {
     this.saveLatestVersionDetails()
   }
 
+  private readonly logger = new Logger(AppController.name)
+
   async saveLatestVersionDetails(): Promise<void> {
+    this.logger.log("Getting app's latest version details")
     const latestSavedVersionDetails = await this.appService.getlatestVersionDetails()
 
     if (
       latestSavedVersionDetails.version !==
       this.configService.get<AppConfig>(APP).version
     ) {
+      this.logger.log("App's version is different")
+
+      this.logger.log("Saving app's latest version details")
       await this.appService.saveVersionDetails(
         new AppDto(this.configService.get<AppConfig>(APP)),
       )
+    } else {
+      this.logger.log("App's latest version details is up-to-date")
     }
   }
 }
--- src/app/app.resolver.ts
+++ src/app/app.resolver.ts
@@ -1,3 +1,4 @@
+import { Logger } from "@nestjs/common"
 import { Resolver, Query } from "@nestjs/graphql"
 import { AppService } from "./app.service"
 import { App } from "./entities/app.entity"
@@ -5,9 +6,16 @@ import { App } from "./entities/app.entity"
 @Resolver(() => App)
 export class AppResolver {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppResolver.name)
 
   @Query(() => App)
   latesVersionDetails(): Promise<App> {
+    this.logger.log(
+      this.latesVersionDetails.name,
+      `${AppResolver.name} - Query`,
+    )
+
+    this.logger.log("Getting and returning app's latest version details")
     return this.appService.getlatestVersionDetails()
   }
 }
